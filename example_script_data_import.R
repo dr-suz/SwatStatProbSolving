637a82834a2b25878851005c77620c7a4f59252f
ST <thornton.suzy@gmail.com>
2023-03-20 20:02:25 -0400

    ex code

--- example_script_data_import.R
+++ example_script_data_import.R
@@ -20,3 +20,33 @@ load(file="38090-0002-Data.rda")
 load(file="38090-0003-Data.rda")
 load(file="38090-0004-Data.rda")
 load(file="38090-0005-Data.rda")
+
+## Give the data files better names 
+dat1 <- da38090.0001
+dat2 <- da38090.0002
+dat3 <- da38090.0003
+dat4 <- da38090.0004
+dat5 <- da38090.0005
+
+
+
+## Here's some examples of variables that may be of interst: 
+
+names(dat2)
+# V2025B # HH bldng with restricted access
+summary(dat2$V2026) # household income 
+summary(dat2$V2031) # race of household head 
+summary(dat2$V2045) # reference person sex
+summary(dat2$V2049A) # reference person race 
+summary(dat2$V2120) # public housing 
+summary(dat2$V2122) # family structure
+
+summary(dat3$V3017) # sex 
+summary(dat3$V3023A) # race 
+summary(dat3$V3081) # # of crimes 
+summary(dat3$V3086) # gender identity 
+summary(dat3$V3084) # sex orientation 
+
+summary(dat3$V3034) # theft 
+summary(dat3$V3040) # theft attack or threat 
+summary(dat3$V3042) # attack treat weapon
